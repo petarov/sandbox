8744ab68f214a598a974c96c2fc9d7949ff8464f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-10-24 23:40:37 +0200

    Extended SSE example with more info and functionality

---
+++ js/eventsource/README.md
@@ -0,0 +1,7 @@
+## Server-Sent Events example
+
+### Tutorials
+
+  * https://developer.mozilla.org/en-US/docs/Server-sent_events
+  * https://developer.mozilla.org/en-US/docs/Server-sent_events/Using_server-sent_events
+  * http://www.html5rocks.com/en/tutorials/eventsource/basics/
--- js/eventsource/server.js
+++ js/eventsource/server.js
@@ -6,17 +6,38 @@ var http = require('http')
  
 http.createServer(function (req, res) {
     if (req.url == '/events') {
-        res.writeHead(200, { 'Content-Type'  : 'text/event-stream'
-                         , 'Cache-Control' : 'no-cache'
-                         //, 'Connection'    : 'keep-alive'
+        res.writeHead(200, { 'Content-Type': 'text/event-stream'
+                         , 'Cache-Control': 'no-cache'
+                         , 'Connection': 'keep-alive'
                          });
 
         console.log('Client connect');
 
+        var colors = ['#4bd88e', '#9eda06', '#63c7e4', '#b1a997', '#f509ee', '#28b902', '#dc8508' ];
+
+// The browser attempts to reconnect to the source roughly 3 seconds after each
+// connection is closed. You can change that timeout by including a line
+// beginning with "retry:", followed by the number of milliseconds to wait before
+// trying to reconnect.
+
         var t = setInterval(function () {
             console.log('Send data');
-            res.write('data: DATA\n\n');
+            var idx = Math.floor(Math.random() * colors.length);
+            res.write('retry: 5000\n');
+            res.write('data: ' + colors[idx] + '\n\n');
         }, 1000);
+
+        var t2 = setInterval(function () {
+            console.log('Send ping');
+
+            var date = new Date();
+            var data = {
+                'time': date.toUTCString()
+            }
+            res.write('event: ping\n');
+            res.write('retry: 8000\n');
+            res.write('data: ' + JSON.stringify(data) + '\n\n');
+        }, 2000);        
 
         res.socket.on('close', function () {
             console.log('Client leave');
