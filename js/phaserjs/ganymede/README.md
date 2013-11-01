c4c58598e1dc3149663ed8fcaf13fea64325be3b
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-11-02 00:49:10 +0100

    Added ganymede PhaserJS game prototype

---
+++ js/phaserjs/ganymede/README.md
@@ -0,0 +1,16 @@
+_.o0 Ganymede 0o._
+==========================
+
+![alt text](http://i.imgur.com/Dl4CHyT.png "Ganymede")
+
+HTML5/Javascript web browser game. 
+
+# Controls
+Collect as many carrots as possible in **3 minutes**. Use the controls to move and repel opponents.
+
+  * **Arrow keys** - Move ship
+  * **Z (or Y)** - Shoot
+
+# License
+The game **without game art** is under [MIT License](http://opensource.org/licenses/MIT). 
+Read the [LICNESE](LICENSE) file for more info.
---
+++ js/phaserjs/ganymede/src/launcher.js
@@ -0,0 +1,53 @@
+(function () {
+
+    var game = new Phaser.Game(640, 360, Phaser.AUTO, 'canvas', { preload: preload, create: create, update: update });
+    var background;
+    var ship;
+
+    var xacc = 0;
+    var yacc = 0;
+    var speed = 0.2;
+    var dec = speed / 2.0;
+
+    function preload() {
+        game.load.image('backgorund', 'assets/background.png');
+        game.load.image('ship', 'assets/spaceship_engine_sharp.png');
+    }
+
+    function create() {
+
+        background = game.add.tileSprite(0, 104, 640, 360, 'backgorund');
+        ship = game.add.sprite(0, 48, 'ship');
+
+    }
+
+    function update() {
+
+        background.tilePosition.x -= 2;
+
+        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            xacc -= speed;
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            xacc += speed;
+        }
+
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            yacc -= speed;
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            yacc += speed;
+        }
+
+        if (xacc > 0)
+            xacc -= dec;
+        if (xacc < 0)
+            xacc += dec;
+       if (yacc > 0)
+            yacc -= dec;
+        if (yacc < 0)
+            yacc += dec;        
+
+        ship.x += xacc;
+        ship.y += yacc;
+    }
+
+})();
