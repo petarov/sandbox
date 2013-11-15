0f8b5aeae40b9d8a434fcd2f5b6bf41bbd3306ab
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-11-16 00:27:08 +0100

    Added ellastic collision of enemy cubes

--- game-jams/rectyx/src/enemy.js
+++ game-jams/rectyx/src/enemy.js
@@ -30,11 +30,88 @@ Crafty.c('Enemy', {
 
 var enemy = {
 
-    add: function() {
+    positions: {
+        topLeft: 'topleft',
+        topRight: 'topRight',
+        bottomLeft: 'bottomLeft',
+        bottomRight: 'bottomRight'
+    },
 
+    enemies: [],
+
+    add: function(where, vSpeed) {
+
+        var vPos = {};
+        switch(where) {
+            case this.positions.topLeft:
+                vPos.x = 5; vPos.y = 5;
+            break;
+            case this.positions.topRight:
+                vPos.x = _Globals.ScreenWidth - 55; vPos.y = 5;
+            break;
+            case this.positions.bottomLeft:
+                vPos.x = 5; vPos.y = _Globals.ScreenHeight - 50 - 5;
+            break;
+            case this.positions.bottomRight:
+                vPos.x = _Globals.ScreenWidth - 55; vPos.y = _Globals.ScreenHeight - 50 - 5;
+            break;
+        }
+
+        if (typeof vSpeed === 'undefined') {
+            vSpeed = {x: 0.5, y: 0.5};
+        }
+
+        var entity = Crafty.e("2D, " + _Globals.render + ", qube, enemy, Color, Tint, Collision")
+        .attr({
+            x: vPos.x, 
+            y: vPos.y, 
+            z: _Globals.zbase + 1,
+            w: 50, h: 50,
+            xspeed: vSpeed.x,
+            yspeed: vSpeed.y
+        })
+        .origin('center')
+        .color("#00aaa2")
+        .bind("EnterFrame", function() {
+            this.x += this.xspeed;
+            this.y += this.yspeed;
+
+            if (this.x < 0) {
+                this.xspeed = -this.xspeed;
+            } else if (this.x + this.w > _Globals.ScreenWidth) {
+                this.xspeed = -this.xspeed;
+            }
+            if (this.y - 1 < 0) {
+                this.yspeed = -this.yspeed;
+            } else if (this.y + this.h > _Globals.ScreenHeight) {
+                this.yspeed = -this.yspeed;
+            }
+
+            var hits = this.hit('qube');
+            if (hits) {
+                var e = hits[0].obj;
+                var tmpx = this.xspeed;
+                var tmpy = this.yspeed;
+                this.xspeed = e.xspeed;
+                this.yspeed = e.yspeed;
+                e.xspeed = tmpx;
+                e.yspeed = tmpy;
+            }
+        })
+        .bind("damage", function(dmg) {
+            this.hp -= dmg;
+            if (this.hp <= 0) {
+                $('#instructions').html('YOU ARE DEAD!');
+                Crafty.stop();
+            }
+        });
+
+        this.enemies.push(entity);
     },
 
     removeAll: function() {
-
+        for (var i = enemies.length - 1; i >= 0; i--) {
+            this.enemies[i].destory();
+        }
     }
 };
--- game-jams/rectyx/src/game.js
+++ game-jams/rectyx/src/game.js
@@ -4,7 +4,6 @@
  * https://github.com/petarov/sandbox/tree/master/game-jams/rectyx
  */
 
-// main scene
 Crafty.scene("game", function() {
 
 	var render = _Globals.render;
@@ -14,46 +13,11 @@ Crafty.scene("game", function() {
     .bind("MessureFPS", function(fps) {
         $('#fps').text('FPS: ' + fps.value);
     });
-    
-    var zbase = 2;
 
-    // create character
-    var player = Crafty.e("2D, " + render + ", player, Color, Multiway")
-    .attr({
-        move: {left: false, right: false, up: false, down: false},
-        x: 304, y: 209, z: zbase + 1,
-        moving: false,
-        curAngle: 0,
-        hp: 100,
-        kills: 0,
-        w: 50, h: 50
-    })
-    .origin('center')
-    .color("#FA5656")
-    .bind("Moved", function(from) {
-        this.moving = true;
-        if (this.x < 0) {
-            this.x = from.x;
-        } else if (this.x + this.w > _Globals.ScreenWidth) {
-            this.x = from.x;
-        }
-        if (this.y - 1 < 0) {
-            this.y = from.y;
-        } else if (this.y + this.h > _Globals.ScreenHeight) {
-            this.y = from.y;
-        }
-    })
-    .bind("EnterFrame", function() {
-        // If moving, adjust the proper animation and facing
-        // 
-    })
-    .multiway(2, {W: -90, S: 90, D: 0, A: 180})
-    .bind("damage", function(dmg) {
-        this.hp -= dmg;
-        if (this.hp <= 0) {
-            $('#instructions').html('YOU ARE DEAD!');
-            Crafty.stop();
-        }
-    });
+ 
+
+    enemy.add(enemy.positions.topLeft);
+    enemy.add(enemy.positions.topRight, {x: 2.5, y: 2.5});
+    enemy.add(enemy.positions.bottomRight, {x: 1.5, y: 2.5});
 
 });
