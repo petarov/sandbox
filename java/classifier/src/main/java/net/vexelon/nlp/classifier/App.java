c8d478e41bf0a5456dc64059f037d3826f888d5d
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-04-04 21:49:40 +0200

    Refactored test files load procedure

--- java/classifier/src/main/java/net/vexelon/nlp/classifier/App.java
+++ java/classifier/src/main/java/net/vexelon/nlp/classifier/App.java
@@ -4,11 +4,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,27 +39,24 @@ public class App
     
     public void start() throws Exception {
     	
-    	String[] testFiles = {
-    			"data/test/Cosmic barometer could reveal violent events in universe's past.txt",
-    			"data/test/Mars-mimicking chamber explores habitability of other planets.txt",
-    			"data/test/NASA Mars Orbiter images may show 1971 Soviet lander.txt",
-    			"data/test/Observatory.txt",
-    			"data/test/Space tool for lunar exploration.txt"
-    	};
-    	
     	List<DoccatModel> models = loadModels();
-    	
-    	for (String path : testFiles) {
-    		log.info("----- Testing: {} ------", path);
-			String contents = readFile(path);
-			categorize(contents, models);
-		}
-    	
+    
+    	// test all files ending on .txt in the data/test folder
+    	try (DirectoryStream<Path> ds = Files.newDirectoryStream(FileSystems.getDefault().getPath("data/test"))) {
+    		for (Path file : ds) {
+    			String ext = FilenameUtils.getExtension(file.getFileName().toString());
+    			if (ext.equals("txt")) {
+	        		log.info("----- Testing: {} ------", file);
+	    			String contents = readFile(file);
+	    			categorize(contents, models);    	
+    			}
+    		}
+    	}
     }
     
-    private String readFile(String filePath) throws IOException {
-    	List<String> lines = Files.readAllLines(FileSystems.getDefault().getPath(filePath), Charset.forName("utf-8"));
-    	log.trace("Lines Read: {}", lines.size());
+    private String readFile(Path filePath) throws IOException {
+    	List<String> lines = Files.readAllLines(filePath, Charset.forName("utf-8"));
+    	log.trace("Lines read: {}", lines.size());
     	
     	String text = StringUtils.join(lines.toArray());
     	return text;
@@ -81,22 +81,14 @@ public class App
     			"data/en-comets.bin",
     			"data/en-mars.bin"
     	};
-    	
-    	InputStream is = null;
-    	try {
     		
-    		for (String path : modelFiles) {
-    			log.info("Loading model: {}", path);
-    			is = new FileInputStream(path);
-    			DoccatModel model = new DoccatModel(is);
-    			modelsList.add(model);
+		for (String path : modelFiles) {
+			log.info("Loading model: {}", path);
+			try (InputStream is = new FileInputStream(path)) {
+				DoccatModel model = new DoccatModel(is);
+				modelsList.add(model);
 			}
-    		
-    	} finally {
-    		if (is != null) {
-    			is.close();
-    		}
-    	}
+		}
     	
     	return modelsList;
     }
