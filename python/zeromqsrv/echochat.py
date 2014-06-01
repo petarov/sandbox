c23f677f15eaee605e17d67a48b5daaffd5ce9af
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-06-01 23:58:10 +0200

    Tabs 2 spaces

--- python/zeromqsrv/echochat.py
+++ python/zeromqsrv/echochat.py
@@ -13,29 +13,29 @@ def log(msg):
     sys.stdout.flush()
 
 def timeNow():
-	return time.strftime('[%H:%M:%S]', time.gmtime())
+    return time.strftime('[%H:%M:%S]', time.gmtime())
 
 
 def listen(port):
-	log('Server listening on port %s' % port);
-	ctx = zmq.Context() #.instance()
-	socket = ctx.socket(zmq.REP)
-	socket.bind("tcp://*:%s" % port)
+    log('Server listening on port %s' % port);
+    ctx = zmq.Context() #.instance()
+    socket = ctx.socket(zmq.REP)
+    socket.bind("tcp://*:%s" % port)
 
-	while True:
-		msg = socket.recv_string()
-		log(timeNow() + " Stranger: %s " % msg)
-		time.sleep(1)
-		socket.send_string('Hey! ' + msg)
+    while True:
+        msg = socket.recv_string()
+        log(timeNow() + " Stranger: %s " % msg)
+        time.sleep(1)
+        socket.send_string('Hey! ' + msg)
 
 def send(port, msg):
-	ctx = zmq.Context()
-	socket = ctx.socket(zmq.REQ)
-	socket.connect("tcp://localhost:%s" % port)
-	# socket.send("%s" % msg)
-	socket.send_string(msg)
-	reply = socket.recv_string()
-	log(timeNow() + ' Stranger: %s' % reply)
+    ctx = zmq.Context()
+    socket = ctx.socket(zmq.REQ)
+    socket.connect("tcp://localhost:%s" % port)
+    # socket.send("%s" % msg)
+    socket.send_string(msg)
+    reply = socket.recv_string()
+    log(timeNow() + ' Stranger: %s' % reply)
 
 
 ### main
@@ -44,24 +44,24 @@ port = 7000
 cmd = sys.argv[1] if len(sys.argv) > 1 else ""
 
 if cmd.isdigit():
-	port = cmd
-	cmd = sys.argv[2] if len(sys.argv) > 2 else ""
+    port = cmd
+    cmd = sys.argv[2] if len(sys.argv) > 2 else ""
 
 if cmd == 'send':
-	log('Connecting to port %s ...' % port);
-	msg = sys.argv[2]
-	send(port, msg)
+    log('Connecting to port %s ...' % port);
+    msg = sys.argv[2]
+    send(port, msg)
 elif cmd == 'chat':
-	log('Connecting to port %s ...' % port);
-	log("")
-	log("Chat Mode. Type /quit to leave.")
-	log("")
-	while True:
-		msg = input("[You] ")
-		if msg == '/exit' or msg == '/quit':
-			sys.exit();
+    log('Connecting to port %s ...' % port);
+    log("")
+    log("Chat Mode. Type /quit to leave.")
+    log("")
+    while True:
+        msg = input("[You] ")
+        if msg == '/exit' or msg == '/quit':
+            sys.exit();
 
-		send(port, msg)
-		time.sleep(1)
+        send(port, msg)
+        time.sleep(1)
 else:
-	listen(port)
+    listen(port)
