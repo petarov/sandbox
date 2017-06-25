e41318742ce9806e3740e72707dfe8e72199ec59
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-06-25 22:07:03 +0200

    Add techstack Redux example (initial commit)

---
+++ js/react-native/tech_stack/src/reducers/LibraryReducer.js
@@ -0,0 +1,4 @@
+// list libs reducer
+import data from './LibraryList.json';
+
+export default () => data;
---
+++ js/react-native/tech_stack/src/reducers/index.js
@@ -0,0 +1,7 @@
+// our reducers
+import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
+
+export default combineReducers({
+  libraries: LibraryReducer
+});
