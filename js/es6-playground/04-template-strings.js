67506fe60b74f375ab42ea16046e45aaa362c3a3
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-06-29 22:39:21 +0200

    Add es6 template strings example

---
+++ js/es6-playground/04-template-strings.js
@@ -0,0 +1,10 @@
+// 04 - Template strings provide syntactic sugar for constructing strings.
+
+console.log(`Simlpe '\n' literal!`);
+
+var kilos = 100.5, what = 'bananas';
+var text = `He bought ${kilos} of ${what}!`;
+console.log(text);
+
+var m = 5600, c = 299792458;
+console.log(`E=${m *c}`);
