ed3526a5f318bcb0f890683c437bb4f450457bd2
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-07-03 22:22:57 +0200

    Add es6 Unicode examples

---
+++ js/es6-playground/09-unicode.js
@@ -0,0 +1,24 @@
+// 09 - full Unicode support
+
+// same as ES5.1
+"𠮷".length == 2
+
+// new RegExp behaviour, opt-in ‘u’
+"𠮷".match(/./u)[0].length == 2
+
+// new form
+"\u{20BB7}"=="𠮷"=="\uD842\uDFB7"
+
+// new String ops
+"𠮷".codePointAt(0) == 0x20BB7
+
+// for-of iterates code points
+for(var c of "𠮷") {
+  console.log(c);
+}
+
+for (let l of "Имамногоиразличнихора") {
+  console.log(`CYR: ${l}`);
+}
+
+// eof
