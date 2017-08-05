4f341e79f7bdd5ae2cc283362c6c5d17d5450bd0
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-08-05 15:57:39 +0200

    ipb: Save all posts to markdown file archive

--- python/ipb-thrasher/ipb.py
+++ python/ipb-thrasher/ipb.py
@@ -1,9 +1,10 @@
-
 #!/usr/bin/env python
 # pylint: disable=C0111
 # pylint: disable=C0103
 # pylint: disable=C0330
 # pylint: disable=W0621
+# ------------------------------------------------------------------------
+# Automated posts archiving and removal for Invision Power Board v1.x
 
 from __future__ import print_function
 import os
@@ -14,6 +15,8 @@ import traceback
 import json
 import requests
 from lxml import html
+from lxml import etree
+import re
 
 POSTS_PER_BATCH = 25
 
@@ -51,31 +54,53 @@ def get_session(config):
         return session
 
 def close_session(config, session):
-    print ('Logging user out ...')
-    session.get(URL_LOGOUT.format(config['server_url']))
+    if (config and session):
+        print ('Logging user out ...')
+        session.get(URL_LOGOUT.format(config['server_url']))
 
-def get_posts(config, session, page_posts=0):
+def get_topics(config, session, posts_batch=0):
     response = session.get(URL_LIST_POSTS.format(
-        config['server_url'], config['search_id'], page_posts))
+        config['server_url'], config['search_id'], posts_batch))
+
+    extracted = {}
 
     body = html.fromstring(response.content)
-    posts = body.xpath('//td[@class="row4"]/a[@class="linkthru"]')
-    extracted = []
+    #posts = body.xpath('//td[@class="row4"]/a[@class="linkthru"]')
+    el_posts = body.xpath('//div[@class="tableborder"]')
+    for el_post in el_posts:
+        title = el_post.xpath('.//div[@class="maintitle"]/a[@class="linkthru"][1]/text()')
+        title = title[-1]
 
-    for p in posts:
+        published_on = el_post.xpath('.//table[@class="tablebasic"]//td[@class="row4"]/strong/text()')
+        published_on = published_on[-1]
+
+        content = el_post.xpath('.//table[@class="tablebasic"]//td[@class="post1" and @align="left" and @width="100%"]')
+        content = content[-1]
+        content = etree.tostring(content, pretty_print=True)
+        #content = re.sub(r'<br>', r'\n', content)
+
         # topic id the post is located in
-        topic_id = p.get('href').split('&t=')
+        el_link = el_post.xpath('.//table[@class="tablebasic"]//a[@class="linkthru"][last()]')
+        el_link = el_link[-1]
+        post_link = etree.tostring(el_link)
+        topic_id = el_link.get('href').split('&t=')
         topic_id = topic_id[1].split('&hl=')
         topic_id = topic_id[0]
 
         # post id
-        post_id = p.text.lstrip('#')
-        print ('Found post: {} in topic: {}'.format(post_id, topic_id))
+        post_id = el_link.text.lstrip('#')
+        print ('Found post: {} in topic: {}'.format(post_id, topic_id))        
 
-        extracted.append({
+        posts = extracted.get(topic_id, [])
+        posts.append({
             'post_id': post_id,
-            'topic_id': topic_id
+            'topic_id': topic_id,
+            'title': title,
+            'published_on': published_on,
+            'content': content,
+            'link': post_link
         })
+        extracted[topic_id] = posts
 
     return extracted
 
@@ -112,39 +137,89 @@ def post_edit(config, session, topic_id, post_id, auth_key, content):
     }
     session.post(URL_EDIT_POST.format(config['server_url']), data=payload)
 
+def md_open(config):
+    file_name = config['user'] + "_ipb_history.md"
+    for i in range(100):
+        if os.path.exists(file_name):
+            file_name = config['user'] + "_ipb_history" + str(i) + ".md"
+        else:
+            break
+
+    md_path = re.sub(r'[^\x00-\x7f]', r'', file_name)
+
+    f = open(md_path, 'w')
+    f.write('IPB Forum History\n=======================\n\n')
+    f.write('User *{}* at forum [{}]({})\n\n'.format(config['user'], 
+        config['server_url'], config['server_url']))
+    return f
+
+def md_append(f, posts):
+    if posts.count > 0:
+        title = posts[-1]['title']
+        f.write("# {}\n".format(title.encode('utf-8')))
+
+        for p in posts:
+            print ('Writing post {} ...'.format(p['post_id']))
+
+            f.write("## [Post - {}]({})\n\n".format(
+                p['link'],
+                p['post_id']))
+            f.write(p['content'].encode('utf-8'))
+            f.write('\n')
+
+def md_close(f):
+    if f:
+        f.close()
+
 #############################################################################
 # Main
 if __name__ == "__main__":
     try:
         g_config = config_load(os.path.join('.', CONFIG_FILE))
         g_session = get_session(g_config)
-        g_posts = get_posts(g_config, g_session)
+        md_file = md_open(g_config)
 
-        g_authkey = ''
+        batch = 0
+        g_topics = get_topics(g_config, g_session)
 
-        for p in g_posts:
-            ## TEST ##
-            if p['post_id'] == '416592':
-                if not g_authkey:
-                    g_authkey = get_auth_key(g_config, g_session, 
-                        topic_id=p['topic_id'],
-                        post_id=p['post_id'])
-                    print ('Found auth key: {}'.format(g_authkey))
+        while g_topics.keys().count > 0:
+            print ('Processing posts batch: {}', batch)
 
-                time.sleep(1)
+            g_authkey = ''
 
-                print ('Editting post - {} ...'.format(p['post_id']))
-                post_edit(g_config, g_session, 
-                    topic_id=p['topic_id'],
-                    post_id=p['post_id'],
-                    auth_key=g_authkey,
-                    content='---')
+            for k in g_topics:
+                md_append(md_file, g_topics.get(k))
 
-                time.sleep(1)
+                ## TEST ##
+                # if t['post_id'] == '416592':
+                #     if not g_authkey:
+                #         g_authkey = get_auth_key(g_config, g_session, 
+                #             topic_id=p['topic_id'],
+                #             post_id=p['post_id'])
+                #         print ('Found auth key: {}'.format(g_authkey))
 
-        close_session(g_config, g_session)
+                #     time.sleep(1)
+
+                    # print ('Editting post - {} ...'.format(p['post_id']))
+                    # post_edit(g_config, g_session, 
+                    #     topic_id=p['topic_id'],
+                    #     post_id=p['post_id'],
+                    #     auth_key=g_authkey,
+                    #     content='---')
+                    # time.sleep(1)
+            
+            ### TODO ###
+            break
+
+            batch += POSTS_PER_BATCH
+            g_topics = get_topics(g_config, g_session, batch)
+            time.sleep(1)
+        
     except NameError as e:
         print('[ERROR] {}'.format(e))
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
         print('[ERROR] {}'.format(e))
+    finally:
+        md_close(md_file)
+        close_session(g_config, g_session)
