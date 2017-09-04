efc6e4f6f985f51b8a34c1cd179c4888f32b48cc
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-09-04 22:42:48 +0200

    Add solved mini-max challenge

---
+++ js/hr-challenges/algo-03-mini-max-sum.js
@@ -0,0 +1,61 @@
+/**
+ * https://www.hackerrank.com/challenges/mini-max-sum
+ * 
+ * Given five positive integers, find the minimum and maximum values that can be 
+ * calculated by summing exactly four of the five integers. Then print the 
+ * respective minimum and maximum values as a single line of two space-separated 
+ * long integers.
+ * 
+ * Input Format
+ * 
+ * A single line of five space-separated integers.
+ * 
+ * Constraints
+ * 
+ * Each integer is in the inclusive range [1, 10^9]
+ * 
+ * Output Format
+ * 
+ * Print two space-separated long integers denoting the respective minimum and 
+ * maximum values that can be calculated by summing exactly four of the five 
+ * integers. (The output can be greater than 32 bit integer.)
+ */
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    let arr = readLine().split(' ');
+    arr = arr.map(Number);
+    
+    // 4 1 5 2 3
+    arr.sort();
+    
+    let a = Array.from(arr).splice(0, 4).reduce(function(sum, value) {
+       return sum + value; 
+    }, 0);
+    let b = Array.from(arr).splice(1, 4).reduce(function(sum, value) {
+       return sum + value; 
+    }, 0);   
+    
+    console.log(a, b)
+}
