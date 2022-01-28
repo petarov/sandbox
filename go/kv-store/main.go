ef3700d4eb4dab3cbd7c85f71ad16f8d781ca7c1
Petar Petrov <ppetrov@vexelon.net>
2022-01-28 22:34:25 +0100

    Add a simple key/value store experiment

---
+++ go/kv-store/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+const (
+	PORT = 5607
+)
+
+var (
+	cache = make(map[string]string)
+	mutx  sync.RWMutex
+)
+
+func put(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte("Error parsing form params"))
+		} else {
+			key := r.Form.Get("key")
+			if len(key) == 0 {
+				w.WriteHeader(400)
+				w.Write([]byte("Missing key"))
+			} else {
+				value := r.Form.Get("value")
+				mutx.Lock()
+				cache[key] = value
+				mutx.Unlock()
+
+				w.WriteHeader(200)
+				w.Write([]byte("OK"))
+			}
+		}
+	}
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(405)
+	} else {
+		keys, ok := r.URL.Query()["key"]
+		if !ok || len(keys) < 1 {
+			w.WriteHeader(400)
+			w.Write([]byte("Missing key"))
+		} else {
+			mutx.RLock()
+			v := cache[keys[0]]
+			mutx.RUnlock()
+
+			if len(v) == 0 {
+				w.WriteHeader(404)
+				w.Write([]byte(fmt.Sprintf("No value found for '%s'", keys[0])))
+			} else {
+				w.WriteHeader(200)
+				w.Write([]byte(v))
+			}
+		}
+	}
+}
+
+func main() {
+
+	fmt.Println("*** Welcome to the mini key/value store! ***")
+	fmt.Printf("*** Listening on: %d ***\n", PORT)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/put", put)
+	mux.HandleFunc("/get", get)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux)
+}
