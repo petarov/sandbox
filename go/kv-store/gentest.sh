4d4c1c98741cd01bbc795a1c3d95be2ea5944749
Petar Petrov <ppetrov@vexelon.net>
2022-01-29 21:17:51 +0100

    Fix generate test files

--- go/kv-store/gentest.sh
+++ go/kv-store/gentest.sh
@@ -5,8 +5,8 @@ POUT=post-urls.txt
 GOUT=get-urls.txt
 MAX=100
 
-> $POUT
-> $GOUT
+rm -f $POUT
+rm -f $GOUT
 
 i=0
 while [[ $i -le $MAX ]]
@@ -18,11 +18,11 @@ do
    KEY3=key$RANDOM
    VAL3=$RANDOM
 
-   echo "$SURL POST $KEY1=$VAL1" >> $POUT
-   echo "$SURL POST $KEY2=$VAL2&$KEY3=$VAL3" >> $POUT
+   echo "$SURL/ POST $KEY1=$VAL1" >> $POUT
+   echo "$SURL/ POST $KEY2=$VAL2&$KEY3=$VAL3" >> $POUT
 
-   echo "$SURL?key=$KEY1 GET" >> $GOUT
-   echo "$SURL?key=$KEY2&key=$KEY3 GET" >> $GOUT
+   echo "$SURL/?key=$KEY1" >> $GOUT
+   echo "$SURL/?key=$KEY2&key=$KEY3" >> $GOUT
 
     ((i = i + 1))
 done
