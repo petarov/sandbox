877c796f4275264244bdd103d90112495a8b0891
Petar Petrov <ppetrov@vexelon.net>
2022-01-29 18:45:12 +0100

    Add testing info

--- go/kv-store/README.md
+++ go/kv-store/README.md
@@ -20,6 +20,14 @@ To get multiple semicolon (;) separated values from the store:
 
     curl -X GET "http://127.0.0.1:5607/?key=name&key=age"
 
+## Testing
+
+Use [siege](https://github.com/JoeDog/siege) and `gentest.sh`.
+
+    siege -c 8 -f post-urls.txt
+    
+    siege -c 8 -f get-urls.txt
+
 # License
 
 WTFPL
---
+++ go/kv-store/gentest.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+SURL="http://127.0.0.1:5607"
+POUT=post-urls.txt
+GOUT=get-urls.txt
+MAX=100
+
+> $POUT
+> $GOUT
+
+i=0
+while [[ $i -le $MAX ]]
+do
+   KEY1=key$RANDOM
+   VAL1=$RANDOM
+   KEY2=key$RANDOM
+   VAL2=$RANDOM
+   KEY3=key$RANDOM
+   VAL3=$RANDOM
+
+   echo "$SURL POST $KEY1=$VAL1" >> $POUT
+   echo "$SURL POST $KEY2=$VAL2&$KEY3=$VAL3" >> $POUT
+
+   echo "$SURL?key=$KEY1 GET" >> $GOUT
+   echo "$SURL?key=$KEY2&key=$KEY3 GET" >> $GOUT
+
+    ((i = i + 1))
+done
+
+echo Done.
